be3f1d18ffa37c49db9ff34b0ac8e9e624163adf
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-07-12 12:44:01 +0100

    db connection

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const URL = "mongodb://127.0.0.1:27017/timetable";
-/* const URL_DEPLOY =
-  "mongodb+srv://vanessafuangi:mfSqtb9jGNN61ZcU@cluster0.kuc3nxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; */
+// const URL = "mongodb://127.0.0.1:27017/timetable";
+const URL_DEPLOY =
+  "mongodb+srv://vanessafuangi:mfSqtb9jGNN61ZcU@cluster0.kuc3nxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
   .connect(URL)
   .then(() => console.log("Connected to Database with success!!"))
